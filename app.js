6a4b0cff90325ee1fcade911f6221b7f058349dc
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-14 16:07:01 +0530

    express js files

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const Express=require('express')
 var app1 = new Express();
-app1.get('/home',(req,res)=>{
-    res.send("welcome to my web");
+app1.set('view engine','ejs');
+
+app1.get('/',(req,res)=>{
+    res.render('index',{title:"ICT","names":["a","b","c"]});
 });
-app1.listen(3000);
+
+app1.get('/contact',(req,res)=>{
+    res.render('contact')
+});
+
+app1.get('/table',(req,res)=>{
+    res.render('table')
+});
+
+app1.get('/home',(req,res)=>{
+    res.send("<h1>welcome to my world</h1>");
+});
+
+
+
+app1.listen(3000,()=>{
+    console.log("server running")
+});
